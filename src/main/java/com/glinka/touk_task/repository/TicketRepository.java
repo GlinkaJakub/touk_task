ae54ce4c0d672ca823acd90f80b43b3c45ddcc79
Jakub Glinka <glinka.jakub@outlook.com>
2021-06-04 13:37:31 +0200

    remove code

--- src/main/java/com/glinka/touk_task/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.glinka.touk_task.repository;
-
-import com.glinka.touk_task.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-}
